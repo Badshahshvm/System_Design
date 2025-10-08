06c2cd358fa4b4ba2e8589ee0a2aeefd9f180948
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-10-08 14:41:29 +0530

    Add files via upload

---
+++ Adapter Designe Pattern/OldPrinter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_Designe_Pattern
+{
+    internal class OldPrinter
+    {
+
+        public void OldPrint(string content)
+        {
+            Console.WriteLine("Old Printer Printing: " + content);
+        }
+    }
+}
---
+++ Adapter Designe Pattern/PrinterAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_Designe_Pattern
+{
+    internal class PrinterAdapter:IPrinter
+    {
+        private readonly OldPrinter _oldPrinter;
+
+        public PrinterAdapter(OldPrinter oldPrinter)
+        {
+            _oldPrinter = oldPrinter;
+        }
+
+        public void Print(string text)
+        {
+            // Adapting the method call
+            _oldPrinter.OldPrint(text);
+        }
+    }
+}
