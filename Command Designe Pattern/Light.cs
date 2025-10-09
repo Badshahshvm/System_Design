4d7bbfe620706f1b137af85b1094448ac6789430
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-10-09 11:24:33 +0530

    Add files via upload

---
+++ Command Designe Pattern/Light.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command_Designe_Pattern
+{
+    internal class Light
+    {
+        public void TurnOn() => Console.WriteLine("Light is ON");
+        public void TurnOff() => Console.WriteLine("Light is OFF");
+    }
+}
---
+++ Command Designe Pattern/TurnOffCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command_Designe_Pattern
+{
+    internal class TurnOffCommand:ICommand
+    {
+        private Light _light;
+        public TurnOffCommand(Light light)
+        {
+            _light = light;
+        }
+
+        public void execute()
+        {
+            _light.TurnOff();
+        }
+    }
+}
